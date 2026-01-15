ef63119dd56fd768acb718b414311f725caaa2c1
TinLaiLapTrinh <142028792+TinLaiLapTrinh@users.noreply.github.com>
2026-01-15 23:06:25 +0700

    Update settings.py

--- Backend/GScore/GScore/settings.py
+++ Backend/GScore/GScore/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eid-2s-$5zpu7&-y&b35kd6&^51pi^zp6y)k0odzr=ja-5%r*6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
